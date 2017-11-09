7a0767c7dd4acba767beb28fededc1dd5d50ec77
rain <tangmt2@126.com>
2017-11-09 16:28:23 +0800

    recursion的定义写好了

--- complier/recursion.cpp
+++ complier/recursion.cpp
@@ -1,3 +1,37 @@
 #include <iostream>
 
 using namespace std;
+
+class recursion
+{
+public:
+	int static program();//1
+	int static declaration_list();
+	int static declaration();
+	int static var_declaration();
+	int static compound_stmt();//5
+	int static fun_declaration();
+	int static params();
+	int static param_list();
+	int static param();
+	int static compound_stmt();//10
+	int static local_declarations();
+	int static statement_list();
+	int static statement();
+	int static expression_stmt();
+	int static selection_stmt();//15
+	int static iteration_stmt();
+	int static return_stmt();
+	int static expression();
+	int static var();
+	int static simple_expression();//20
+	int static relop();
+	int static additive_expression();
+	int static addop();
+	int static term();
+	int static mulop();//25
+	int static factor();
+	int static call();
+	int static args();
+	int static arg_list();//29
+};
