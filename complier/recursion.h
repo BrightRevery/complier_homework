03412c251449ad28f247d531288326300f44fc51
rain <tangmt2@126.com>
2017-11-09 19:55:44 +0800

    修改了c minus现在是通用的了

--- complier/recursion.h
+++ complier/recursion.h
@@ -1,33 +1,33 @@
 class recursion
 {
 public:
-	int static program();//1
+	int static program();
 	int static declaration_list();
 	int static declaration();
 	int static var_declaration();
-	int static compound_stmt();//5
+	int static compound_stmt();
 	int static fun_declaration();
 	int static params();
 	int static param_list();
 	int static param();
-	int static compound_stmt();//10
+	int static compound_stmt();
 	int static local_declarations();
 	int static statement_list();
 	int static statement();
 	int static expression_stmt();
-	int static selection_stmt();//15
+	int static selection_stmt();
 	int static iteration_stmt();
 	int static return_stmt();
 	int static expression();
 	int static var();
-	int static simple_expression();//20
+	int static simple_expression();
 	int static relop();
 	int static additive_expression();
 	int static addop();
 	int static term();
-	int static mulop();//25
+	int static mulop();
 	int static factor();
 	int static call();
 	int static args();
-	int static arg_list();//29
+	int static arg_list();
 };
