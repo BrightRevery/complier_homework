476a272d37e066589e686fc0dfbebc4528620609
rain <tangmt2@126.com>
2017-11-09 09:58:00 +0800

    add all

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int morphology();
+	ifstream file_in("target.txt");
+	ofstream file_out("4e.txt");
+	vector<int> trans;
+	morphology();
+	system("pause");
+
+	return 0;
+}
